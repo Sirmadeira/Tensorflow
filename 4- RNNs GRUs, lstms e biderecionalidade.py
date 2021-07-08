1c9aff3b3184ffcde499bc0bb515cb58220afddd
Sirmadeira <71156378+Sirmadeira@users.noreply.github.com>
2021-07-08 15:55:49 -0300

    Update 4- RNNs GRUs, lstms e biderecionalidade.py

--- 4- RNNs GRUs, lstms e biderecionalidade.py
+++ 4- RNNs GRUs, lstms e biderecionalidade.py
@@ -1,6 +1,6 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
@@ -11,3 +11,25 @@ from tensorflow.keras.datasets import mnist
 
 x_train=x_train.astype('float32')/255
 x_test=x_test.astype('float32')/255
+
+
+model = keras.Sequential()
+model.add(keras.Input(shape=(None,28)))
+model.add(
+	layers.Bidirectional(
+		layers.SimpleRNN(256, return_sequences=True,activation='relu')
+		)
+	)
+#Biderectional como discutid na teorica, e repetir o numero de nodes so que ao contrario
+#Para ativar  uma funcao recorrent gru ou lstm e so trocar o nome SIMPLERNN POR GRU LSTM
+#Return sequences,monta um timestep seria o loop interno da data por um node, o que faz dela recurrent. Nesse caso o loop e 28 vezes
+model.add(layers.SimpleRNN(256,activation='relu'))
+#Nesse caso nao nomeie return sequences, logo ela somente faz um timestep com os inputs anteriores, sem rotacionar eles
+model.add(layers.Dense(10))
+model.compile(
+	loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+	optimizer=keras.optimizers.Adam(lr=0.001),
+	metrics=['accuracy']
+	)
+model.fit(x_train, y_train,batch_size=64,epochs=10,verbose=2)
+model.evaluate(x_test.astype,batch_size=64,verbose=2)
